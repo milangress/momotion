385a1936beb32014f98de25c6bfb97da5cd7cff9
MilanG <milan@milan.local>
2017-11-24 20:03:25 +0100

    Websocket and recorder (w/o server handeling)

--- js/work.js
+++ js/work.js
@@ -6,50 +6,94 @@ let xValue;
 let yValue;
 let zValue;
 let movementData;
-let connection = new WebSocket('ws://localhost:8080', ['soap', 'xmpp']);
+var websocket;
+let count;
+let recordInterval;
+
+writeToScreen("js ready");
+
+function recordMotion() {
+
+  websocket.send(JSON.stringify(movementData))
+  if (count > 500) {
+    websocket.send("END");
+    writeToScreen("END recording")
+//    websocket.close();
+    clearInterval(recordInterval);
+  }
+  count += 1;
+}
+
+function startRecordingMotion() {
+  if (websocket.readyState == 1) {
+    count = 0;
+    var audio = new Audio('res/start-beeps.wav');
+    audio.play();
+    setTimeout(function () {
+      websocket.send("START recording")
+      writeToScreen("START recording")
+      recordInterval = setInterval(recordMotion, 10);
+    }, 3800);
+  } else {
+    writeToScreen("no websocket open");
+  }
+}
 
 
 function handleMotionEvent(event) {
 
-    let xValue = event.accelerationIncludingGravity.x;
-    let yValue = event.accelerationIncludingGravity.y;
-    let zValue = event.accelerationIncludingGravity.z;
+  let xValue = event.acceleration.x;
+  let yValue = event.acceleration.y;
+  let zValue = event.acceleration.z;
 
 
-  document.querySelector(".xdata") .innerText = xValue;
-  document.querySelector(".ydata") .innerText = yValue;
-    document.querySelector(".zdata") .innerText = zValue;
+  document.querySelector(".xdata").innerText = xValue;
+  document.querySelector(".ydata").innerText = yValue;
+  document.querySelector(".zdata").innerText = zValue;
 
   movementData = {
-    xdata: xValue,
-    ydata: yValue,
-    zdata: zValue
-  }
+      xdata: xValue,
+      ydata: yValue,
+      zdata: zValue
+    }
+    //  writeToScreen(JSON.stringify(movementData));
 
-  connection.send(JSON.stringify(movementData))
 
 }
 
 if (window.DeviceMotionEvent) {
   window.addEventListener('devicemotion', handleMotionEvent);
-  document.querySelector("#message").innerText = "support device motion";
+  writeToScreen("support device motion");
 
 }
 
+websocket = new WebSocket("ws://milan.local:8080");
 
-  // When the connection is open, send some data to the server
-  connection.onopen = function () {
-  connection.send('ConnectionOpen'); // Send the message 'Ping' to the server
+
+
+// When the connection is open, send some data to the server
+websocket.onopen = function () {
+  websocket.send('ConnectionOpen'); // Send the message 'Ping' to the server
 };
 
 
 // Log errors
-connection.onerror = function (error) {
-  console.log('WebSocket Error ' + error);
+websocket.onerror = function (error) {
+  writeToScreen('WebSocket Error ' + error);
 };
 
 // Log messages from the server
-connection.onmessage = function (e) {
+websocket.onmessage = function (e) {
   console.log('Server: ' + e.data);
-  document.querySelector("#message").innerText += " & Server: " + e.data
+  writeToScreen(e.data);
 };
+
+
+
+function writeToScreen(message) {
+  var output = document.getElementById("message");
+  var pre = document.createElement("p");
+  pre.style.wordWrap = "break-word";
+  pre.innerHTML = message;
+  output.appendChild(pre);
+}
