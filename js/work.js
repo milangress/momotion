97c278283a78969e7c1b7ee93b5fb7baf123c314
MilanG <milan@milan.local>
2017-11-24 16:07:22 +0100

    working websockets

--- js/work.js
+++ js/work.js
@@ -5,9 +5,9 @@
 
 function handleMotionEvent(event) {
 
-    var x = event.accelerationIncludingGravity.x;
-    var y = event.accelerationIncludingGravity.y;
-    var z = event.accelerationIncludingGravity.z;
+    let x = event.accelerationIncludingGravity.x;
+    let y = event.accelerationIncludingGravity.y;
+    let z = event.accelerationIncludingGravity.z;
 
 
   document.querySelector(".xdata") .innerText = x;
@@ -20,3 +20,20 @@ if (window.DeviceMotionEvent) {
   document.querySelector("#message").innerText = "support device motion";
 
 }
+
+
+let connection = new WebSocket('ws://localhost:8080', ['soap', 'xmpp']);
+// When the connection is open, send some data to the server
+connection.onopen = function () {
+  connection.send('Ping'); // Send the message 'Ping' to the server
+};
+
+// Log errors
+connection.onerror = function (error) {
+  console.log('WebSocket Error ' + error);
+};
+
+// Log messages from the server
+connection.onmessage = function (e) {
+  console.log('Server: ' + e.data);
+};
