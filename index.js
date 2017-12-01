1795379c24ab1e9399add1dbfe412ef04e44080a
MilanG <milan@milan.local>
2017-12-01 21:05:23 +0100

    heroku test

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
