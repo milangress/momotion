d02d6c8a92a999b350f9edb531143f7ca002f1d5
MilanG <milan@milan.local>
2017-12-01 21:15:07 +0100

    heroku test

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
+  .get('/', (req, res) => res.render('index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
